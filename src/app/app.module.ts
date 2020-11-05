e581ee7a73a105eb16d3ba3fee538ebffa03ce8e
lani <lanimorton356@gmail.com>
2020-11-04 21:59:18 -0800

    Learning about directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleDirective } from './learning-about-directives/attribute-directives/simple.directive';
+import { DirectivesComponent } from './learning-about-directives/directives/directives.component';
+import { CustomizableDirective } from './learning-about-directives/attribute-directives/customizable.directive';
+import { CustomizableShorthandDirective } from './learning-about-directives/attribute-directives/customizable-shorthand.directive';
+import { CustomizableShorthandReadableDirective } from './learning-about-directives/attribute-directives/customizable-shorthand-readable.directive';
+import { WhatsWithTheAsteriskComponent } from './learning-about-directives/whats-with-the-asterisk/whats-with-the-asterisk.component';
+import { NgSwitchComponent } from './learning-about-directives/ng-switch/ng-switch.component';
+import { HappyRobotComponent } from './learning-about-directives/whats-with-the-asterisk/happy-robot/happy-robot.component';
+import { HungryRobotComponent } from './learning-about-directives/whats-with-the-asterisk/hungry-robot/hungry-robot.component';
+import { TiredRobotComponent } from './learning-about-directives/whats-with-the-asterisk/tired-robot/tired-robot.component';
+import { LostRobotComponent } from './learning-about-directives/whats-with-the-asterisk/lost-robot/lost-robot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleDirective,
+    DirectivesComponent,
+    CustomizableDirective,
+    CustomizableShorthandDirective,
+    CustomizableShorthandReadableDirective,
+    WhatsWithTheAsteriskComponent,
+    NgSwitchComponent,
+    HappyRobotComponent,
+    HungryRobotComponent,
+    TiredRobotComponent,
+    LostRobotComponent
   ],
   imports: [
     BrowserModule,
